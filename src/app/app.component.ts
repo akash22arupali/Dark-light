9a52ed65a09858fcbe6c19f0ad97cc5ff1fb02b9
akash22arupali <akasharupali@gmail.com>
2024-06-05 11:26:27 +0530

    Dark light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { DarkModeService } from './services/dark-mode.service';
+import { Inject } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dark-light';
+  darkModeService: DarkModeService=inject(DarkModeService)
 }
